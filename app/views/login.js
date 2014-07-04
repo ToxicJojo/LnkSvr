dfb9339eec76adb3846194b4ebd0870cb201a42e
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2014-07-04 02:33:26 +0200

    Added login and register view.

---
+++ app/views/login.js
@@ -0,0 +1,5 @@
+var show = function(request, response, params) {
+  response.render('login/show', params);
+};
+
+module.exports.show = show;
---
+++ app/views/register.js
@@ -0,0 +1,5 @@
+var show = function(request, response, params) {
+  response.render('register/show', params);
+};
+
+module.exports.show = show;
