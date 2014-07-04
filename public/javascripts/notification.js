6e5a7a4d65d1fb0947af6faf77e9d9107a36301a
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2014-07-04 16:30:52 +0200

    Added a way to display notifications to the user.

---
+++ public/javascripts/notification.js
@@ -0,0 +1,27 @@
+var writeDiv = function(pMessage) {
+  $('#notificationDiv').html(
+    '<div class="alert alert-dismissable" id="notification">' +
+    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+    pMessage +
+    '</button></div>'
+  );
+};
+
+var notification = {
+  showError: function(pErrorMessage) {
+    writeDiv(pErrorMessage);
+    $('#notification').addClass('alert-danger');
+  },
+  showWarning: function(pWarningMessage) {
+    writeDiv(pWarningMessage);
+    $('#notification').addClass('alert-warning');
+  },
+  showInfo: function(pInfoMessage) {
+    writeDiv(pInfoMessage);
+    $('#notification').addClass('alert-info');
+  },
+  showSuccess: function(pSuccessMessage) {
+    writeDiv(pSuccessMessage);
+    $('#notification').addClass('alert-success');
+  }
+};
